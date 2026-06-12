d097c02dc8f2f843694623d96dcd4ad84e50cc82
Beril G <Beril.Guemues@Student.HTW-Berlin.de>
2026-06-12 13:24:36 +0200

    M4

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div id="app">
-    <header>
-      <h1>🍽️ FoodTracker - Meine Produkte</h1>
-    </header>
-    <main>
-      <!-- Hier wird deine neue Produktliste aufgerufen -->
+    <h1>🍽️ FoodTracker - Meine Produkte</h1>
+    <div style="border: 2px solid red; padding: 20px;">
       <ProductList />
-    </main>
+    </div>
   </div>
 </template>
 
 <script setup>
-// Hier importieren wir die Komponente, die wir gerade gebaut haben
 import ProductList from './components/ProductList.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-header {
-  margin-bottom: 30px;
-}
-</style>
