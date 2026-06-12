01e840a0f4c92902f4a47e079f4929a5fbfb36dc
Beril G <Beril.Guemues@Student.HTW-Berlin.de>
2026-06-12 22:02:00 +0200

    M4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <div id="app">
     <h1>🍽️ FoodTracker - Meine Produkte</h1>
-    <div style="border: 2px solid red; padding: 20px;">
-      <ProductList />
-    </div>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-import ProductList from './components/ProductList.vue'
+import { RouterView } from 'vue-router'
 </script>
